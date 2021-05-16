454b9b41e8b38f82fd35d9e741ca322eb79f4f43
Destoky <adestoky@gmail.com>
2021-05-16 17:59:25 +0200

    Draf & Drop + Envoi au click
    Tout est opérationnel dans les 2 sens

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -26,9 +26,6 @@ export default {
   },
   created() {
     bus.$on("choixCarte", (data) => {
-      if(data.copy === 1) {
-        this.update(data.name,[data]);
-      }
       data.copy = 1; //un exemplaire
       this.alreadyInDeck(data.name,[data])
     });
@@ -36,11 +33,6 @@ export default {
   methods: {
     allowDrop(ev) {
       ev.preventDefault();
-    },
-    update(nomdeCarte,[carteApusher]) {
-      let indexTest = this.deckList.findIndex((obj) => obj.name === nomdeCarte);
-      if(indexTest != -1)this.$set(this.deckList[indexTest],"cost", this.deckList[indexTest].cost++);
-      console.log(carteApusher);
     },
     drop(ev) {
       ev.preventDefault();
@@ -55,6 +47,7 @@ export default {
       if (indexTest === -1) {
         this.deckList.push(carteApusher);
       } else {
+        this.$set(this.deckList[indexTest],"cost", this.deckList[indexTest].cost++); //setter pour voir changement
         this.deckList[indexTest].copy = 2;
       }
     }
